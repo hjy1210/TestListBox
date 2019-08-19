3cafcc0c68d5eea2dc06932ade686dc18962ab1a
hjyanghj <hjyanghj@gmail.com>
2019-08-20 06:23:32 +0800

    first commit

---
+++ TestListBox/Item.cs
@@ -0,0 +1,29 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TestListBox
+{
+	public class Item : BindableBase
+	{
+		public Item(string name)
+		{
+			this.name = name;
+			this.description = "";
+		}
+		private string name;
+		public string Name
+		{
+			get { return name; }
+			set { SetProperty(ref name, value); }
+		}
+		private string description;
+		public string Description
+		{
+			get { return description; }
+			set { SetProperty(ref description, value); }
+		}
+	}
+}
---
+++ TestListBox/MainViewModel.cs
@@ -0,0 +1,32 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace TestListBox
+{
+	public class MainViewModel : BindableBase
+	{
+		public MainViewModel()
+		{
+			items = new ObservableCollection<Item>();
+			items.Add(new Item("One"));
+			items.Add(new Item("Two"));
+			items.Add(new Item("Three"));
+		}
+		private ObservableCollection<Item> items;
+		public ObservableCollection<Item> Items
+		{
+			get { return items; }
+			set { SetProperty(ref items, value); }
+		}
+		private int selectedItemIndex;
+		public int SelectedItemIndex
+		{
+			get { return selectedItemIndex; }
+			set { SetProperty(ref selectedItemIndex, value); }
+		}
+	}
+}
